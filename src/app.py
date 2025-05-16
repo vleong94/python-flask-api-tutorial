0164923cc38d1fcddd010273ba8d423a6930c181
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-05-16 16:49:26 +0000

    really done

--- src/app.py
+++ src/app.py
@@ -14,15 +14,15 @@ def hello_world():
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body:", request_body)
-
     todos.append(request_body)
-
     return jsonify(todos), 200
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete:", position)
-    return jsonify(todos)
+    if 0 <= position < len(todos):
+        todos.pop(position)
+    return jsonify(todos), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
